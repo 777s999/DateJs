5fad8b1c7882130561f3200fb53906a9aaad691c
Subrata <124652261+777s999@users.noreply.github.com>
2024-01-30 14:06:22 +0530

    Add files via upload

--- Date/script.js
+++ Date/script.js
@@ -21,6 +21,50 @@ let myTimeStamp = Date.now();
 // console.log(myCreateDate.getTime());
 // console.log(Math.floor(Date.now() / 1000));
 
-let newDate = new Date();
-console.log(newDate);
-console.log(newDate.getMonth());
+// let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDay());
+// `${newDate.getDay()} and the time`;
+
+// newDate.toLocaleString("default", {
+//   weekday: "long",
+//   //timeZone:''
+// });
+
+//Date interlization-->Intl
+
+// const formatter = new Intl.DateTimeFormat("en-US");
+// console.log(formatter.format(new Date()));
+
+// const formatter = new Intl.DateTimeFormat("es", {
+//   //year: "2-digit",
+//   dateStyle: "long",
+// });
+// console.log(formatter.format(new Date()));
+
+// const formatter = new Intl.RelativeTimeFormat("en", { numeric: "always" });
+// console.log(formatter.format(1, "day"));
+
+// const formatter = new Intl.NumberFormat(undefined, {
+//   currency: "INR",
+//   style: "currency",
+// });
+// console.log(formatter.format(321456));
+
+// const formatter = new Intl.NumberFormat(undefined, {
+//   style: "unit",
+//   unit: "liter",
+//unitDisplay:"long"
+// });
+// console.log(formatter.format(35620));
+
+//Social media view code
+// const formatter = new Intl.NumberFormat(undefined, {
+//   notation: "compact",
+// });
+// console.log(formatter.format(14541587896));
+
+const formatter = new Intl.PluralRules(undefined);
+
+console.log(formatter.select(1));
