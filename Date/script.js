17ff33319d0595c42d9c0794349523c0b0edcb41
Subrata <124652261+777s999@users.noreply.github.com>
2024-01-30 12:11:43 +0530

    Add files via upload

---
+++ Date/script.js
@@ -0,0 +1,26 @@
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// let myCreateDate = new Date(2023, 0, 23);
+// let myCreateDate = new Date(2023, 0, 23, 5, 3);
+// console.log(myCreateDate.toLocaleDateString());
+// console.log(myCreateDate.toDateString());
+// console.log(myCreateDate.toLocaleString());
+
+// let myCreateDate = new Date("2023-01-14");
+let myCreateDate = new Date("01-30-2024");
+// console.log(myCreateDate.toLocaleString());
+
+//
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now() / 1000));
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth());
